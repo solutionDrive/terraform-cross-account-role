9cf134d1a8beff52d08b6c014e90d4a5ccc4f044
Matthias Alt <alt@solutiondrive.de>
2018-03-03 14:18:29 +0100

    added description to groups variable to make clear that the name of the group has to be used

--- source_account/variables.tf
+++ source_account/variables.tf
@@ -1,6 +1,7 @@
 variable "account_name" {}
 variable "groups_to_attach" {
   type = "list"
+  description = "List of group names to attach the policy to"
 }
 variable "role_name" {}
 variable "role_arn_in_target_account" {}
